8f4cd5cb2ca7c1936516a13ee24d9135ba730470
emmern1 <emil.nilsen92@gmail.com>
2017-05-26 15:21:29 +0200

    added package.json and webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	entry: __dirname + "/index.js",
+	output: {
+		filename: "bundle.js",
+		path: __dirname + "/assets/",
+		publicPath: "/assets/"
+	},
+	
+	module: {
+		loaders: [
+			{ test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?modules' })},
+		
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['react', 'es2015']
+				}	
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('style.css')
+	]
+}
