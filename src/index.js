b2c83a87d950cc52980e6e9c2a6fcf8f33758580
emmern1 <emil.nilsen92@gmail.com>
2017-05-29 18:18:02 +0200

    font styling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import theme			from './themes.css';
 import Who				from './Who';
 import Parallax			from './ParaLax';
 import Functionality	from './Functionality';
+import NavBar			from './NavBar';
 
 
 
@@ -12,19 +13,24 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<NavBar>
+					<a href="#f">functionality</a>
+					<a href="#w">who's it for</a>
+				</NavBar>
 				<Parallax src="http://stargayzing.com/wp-content/uploads/2014/10/f1fe7d6e7224702e91786e5cb22be905.jpg">
-					<h1>Exhibition Management System<br/>
-						<div style={{
-							display: 'inline-block',
-							height : '20px',
-							width  : '20px',
-							backgroundColor: 'black'
-						}}></div>
+					<h1>
+						<img 
+							style={{height  : '100px'}}
+							src="assets/img/xmslogo.png" />
+						<br/>
+						Exhibition Management System
 					</h1>
 				</Parallax>
-				<Who theme1={theme.blue} theme2={theme.grey}/>
-				<Functionality theme={theme.grey}/>
+				<Who 			id="top" theme1={theme.blue} theme2={theme.grey}/>
+				<Functionality 	id="f" theme={theme.grey}/>
 				<Blank/>
+				<Parallax src="assets/img/cover/1.jpg" small={true} />
+				<Blank />
 				
 				
 			</div>);
