b08d49c02e7b1d095f17f1485714df2c067bc531
emmern1 <emil.nilsen92@gmail.com>
2017-06-12 12:57:15 +0200

    changed cover images

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Functionality		from './Functionality';
 import Who					from './Who';
 
 import NavBar, {ScrollLink}	from './NavBar';
-
+//import Test from './Test';
 
 
 //var aretha = "http://stargayzing.com/wp-content/uploads/2014/10/f1fe7d6e7224702e91786e5cb22be905.jpg";
@@ -25,10 +25,14 @@ class App extends React.Component {
 					<ScrollLink href="functionality">functionality</ScrollLink>
 					<ScrollLink href="who">who's it for</ScrollLink>
 				</NavBar>
-				<Parallax 		id="top"	src="/assets/img/cover/2.jpg" >
+				<Parallax 		id="top"	src="assets/img/cover/4.jpg" >
 					<h1>
 						<img 
-							style={{height  : '100px'}}
+							style={{
+								height  : '100px',
+								filter	: 'invert(1)',
+								
+						}}
 							src="assets/img/xmslogo.png" />
 						<br/>
 						<span className={size.title + ' ' + size.main}>Exhibition Management System</span>
@@ -36,7 +40,7 @@ class App extends React.Component {
 				</Parallax>
 				<Video 								theme={theme.blue}/>
 				<Features 		id="features"		theme={theme.grey}/>
-				<Parallax src="assets/img/cover/3.jpg" small={true} />
+				<Parallax src="assets/img/cover/3_copy_s.jpg" small={true} />
 				<Functionality 	id="functionality"  theme={theme.grey}/>
 				<Who 			id="who" 			theme1={theme.blue} theme2={theme.grey}/>
 				
@@ -50,7 +54,12 @@ class App extends React.Component {
 
 class Blank extends React.Component {
 	render() {
-		return <div style={{height: '100vh', backgroundColor: '#333'}}></div>;
+		return (
+			<div className={theme.contact}>
+				<h1>To find out more about what XMS can do for you contact <a href='mailto:claire@getxms.com'>claire@getxms.com</a>
+				</h1>
+
+			</div>);
 	}
 }
 
@@ -58,5 +67,5 @@ class Blank extends React.Component {
 
 
 ReactDOM.render(
-	<App/>,
+	<App />,
 	document.getElementById('root'))
