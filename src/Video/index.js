67a1fe8c39a4118097f7d76e7297da12441b9531
emmern1 <emil.nilsen92@gmail.com>
2017-06-12 12:57:54 +0200

    using video from vimeo

--- src/Video/index.js
+++ src/Video/index.js
@@ -6,10 +6,11 @@ class Video extends React.Component {
 	render() {
 		return (
 			<div className={css.section + ' ' + this.props.theme}>
-				<video controls>
+				{/*<video controls>
 					<source src='assets/XMS_V004.mp4' type='video/mp4'/>
 					your browser...
-				</video>
+				</video>*/}
+				<iframe src="https://player.vimeo.com/video/220469652" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 			</div>
 			);
 	}
